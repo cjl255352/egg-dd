0f36acd4ee602219f3598ed8c792c79b75557013
J.L.Chen <cjl255352@gmail.com>
2020-05-13 16:05:53 +0800

    update 2020-05-13 16:05

--- config/config.default.js
+++ config/config.default.js
@@ -6,11 +6,8 @@
  */
 exports.dd = {
   client: {
-    // boolean 类型，默认 true，代表应用类型为 “授权服务商开发”
     custom: false,
-    // string 类型，数据加密密钥，用于消息体的加密，长度固定为43个字符，从a-z，A-Z，0-9共62个字符中选取
     encodingAESKey: 'oyBrMEjoGjdoRvjiNWM1cvKZKNOZtsHEKS0BJwl23nd',
-    // string 类型，随机字符串
     token: 'fCDKpSJRUaGHr0vi',
   },
 };
