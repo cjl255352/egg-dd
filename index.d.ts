7cb1af90e246e46ebfb515d63eee643c1e53e628
J.L.Chen <cjl255352@gmail.com>
2020-09-18 17:25:02 +0800

    2020年9月18日17点25分

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 declare module 'egg' {
   interface Application {
     dd: {
-      getDept(id): Promise<void>;
+      getDept(id: number): Promise<void>;
       getDeptList(parentId?: number, recursion?: boolean): Promise<void>;
+      getRoleTree(): Promise<void>
     };
   }
 }
